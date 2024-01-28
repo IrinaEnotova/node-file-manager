29abe1dc21eace8641141f845ac33011ecf1da8a
IrinaEnotova <irinkayashina@gmail.com>
2024-01-28 14:55:31 +0400

    feat: add up and cd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readline from "readline/promises";
 import process from "process";
 import os from "os";
+import path from "path";
 
 import getColorizedText from "./services/cli/getColorizedText.js";
+import getCurrentDir from "./services/cli/getCurrentDir.js";
 
 // create readline
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
@@ -23,13 +25,23 @@ getColorizedText(`Welcome to the File Manager, ${username}!\n`, "message");
 
 // get current homeDir
 let currentDir = os.homedir();
-console.log(`You are currently in ${currentDir}\n`);
+process.chdir(currentDir);
+getCurrentDir(currentDir);
 
 // events
 rl.on("line", (input) => {
-  switch (input) {
+  const command = input.split(" ")[0];
+
+  switch (command) {
     case ".exit":
       rl.close();
+    case "up":
+      changeDirectory("up");
+      break;
+    case "cd":
+      const destPath = input.split(" ")[1];
+      changeDirectory("cd", destPath);
+      break;
     default:
       getColorizedText("Invalid input", "error");
   }
@@ -38,3 +50,29 @@ rl.on("line", (input) => {
 rl.on("close", () => {
   getColorizedText(`Thank you for using File Manager, ${username}, goodbye!`, "message");
 });
+
+export default function changeDirectory(action, destPath = "") {
+  switch (action) {
+    case "up":
+      try {
+        process.chdir(path.join(currentDir, "../"));
+        currentDir = process.cwd();
+        getCurrentDir(currentDir);
+      } catch (err) {
+        getColorizedText("Operation failed", "error");
+      }
+      break;
+    case "cd":
+      if (!destPath) {
+        getColorizedText("You should enter correct pathname", "warn");
+      } else {
+        try {
+          process.chdir(destPath);
+          currentDir = process.cwd();
+          getCurrentDir(currentDir);
+        } catch (err) {
+          getColorizedText("Operation failed", "error");
+        }
+      }
+  }
+}
--- src/services/cli/getColorizedText.js
+++ src/services/cli/getColorizedText.js
@@ -3,6 +3,9 @@ export default function getColorizedText(text, status) {
     case "error":
       console.log("\x1b[31m%s\x1b[0m", text);
       break;
+    case "warn":
+      console.log("\x1b[33m%s\x1b[0m", text);
+      break;
     case "message":
       console.log("\x1b[34m%s\x1b[0m", text);
       break;
